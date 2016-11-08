6f8a7c294b3fdc80caceab460cca68548fee6d55
Kender09 <k.ikeuchi@dcl.cs.waseda.ac.jp>
2016-11-09 00:03:15 +0900

    option time の追加

--- bench/main.go
+++ bench/main.go
@@ -44,6 +44,7 @@ func main() {
   var a_ip = flag.String("a", "localhost", "ipアドレス")
   //var b_ip = flag.String("b", "0.0.0.0", "ipアドレス")
   //var c_ip = flag.String("c", "0.0.0.0", "ipアドレス")
+  var interval_time = flag.String("time", "1s", "ベンチ時間")
   flag.Parse()
 
   var wg *sync.WaitGroup
@@ -57,7 +58,12 @@ func main() {
   a_w.ip = *a_ip
   a_w.benchData.data = map[string]*profData{}
 
-  endtime := time.Now().Add(1*time.Second)
+  duration, err := time.ParseDuration(*interval_time)
+  if err != nil {
+    fmt.Println(err)
+    panic(err)
+  }
+  endtime := time.Now().Add(duration)
   a_w.work(endtime, wg)
   wg.Wait()
 
